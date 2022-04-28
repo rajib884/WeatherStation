15a2527febd66de51e617a0c216b6acf26940504
Rajibul <rajibridoy884@gmail.com>
2022-04-28 13:55:59 +0600

    Created a simple database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import pytz
 
-# Create your models here.
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+
+class DataPoint(models.Model):
+    date = models.DateTimeField("Date Collected", auto_created=True, primary_key=True)
+    temperature = models.FloatField("Temperature")
+    humidity = models.IntegerField("Humidity")
+    pressure = models.IntegerField("Pressure")
+
+    def __str__(self):
+        return self.date.astimezone(pytz.timezone("Asia/Dhaka")).strftime("%Y-%m-%d %H:%M:%S GMT%Z")
